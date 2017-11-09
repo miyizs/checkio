e1578c35767728f4e1190f5171c893a5c82251b9
殷昭姝RoxYin <yinzhaoshu@gznb.com>
2017-11-09 15:32:24 +0800

    non-unique-elements

--- most-wanted-letter.py
+++ most-wanted-letter.py
@@ -12,3 +12,15 @@ def checkio(text):
             tempStr = item[1]
 
     return tempStr
+
+# import string
+#
+# def checkio(text):
+#     """
+#     We iterate through latyn alphabet and count each letter in the text.
+#     Then 'max' selects the most frequent letter.
+#     For the case when we have several equal letter,
+#     'max' selects the first from they.
+#     """
+#     text = text.lower()
+#     return max(string.ascii_lowercase, key=text.count)
